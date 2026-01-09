2cb12f85767c04bee1d09f445f21249fa05b97a2
sam-young717 <syoung@syoung-pc.>
2026-01-09 11:33:36 -0500

    Plz

---
+++ build/CMakeFiles/ATM.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/syoung/Documents/RunnerCT/actions-runner/_work/CTDemo/CTDemo/ATM.cpp" "CMakeFiles/ATM.dir/ATM.cpp.o" "gcc" "CMakeFiles/ATM.dir/ATM.cpp.o.d"
+  "/mnt/c/Users/syoung/Documents/RunnerCT/actions-runner/_work/CTDemo/CTDemo/Account.cpp" "CMakeFiles/ATM.dir/Account.cpp.o" "gcc" "CMakeFiles/ATM.dir/Account.cpp.o.d"
+  "/mnt/c/Users/syoung/Documents/RunnerCT/actions-runner/_work/CTDemo/CTDemo/Bank.cpp" "CMakeFiles/ATM.dir/Bank.cpp.o" "gcc" "CMakeFiles/ATM.dir/Bank.cpp.o.d"
+  "/mnt/c/Users/syoung/Documents/RunnerCT/actions-runner/_work/CTDemo/CTDemo/BaseDisplay.cpp" "CMakeFiles/ATM.dir/BaseDisplay.cpp.o" "gcc" "CMakeFiles/ATM.dir/BaseDisplay.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/syoung/Documents/RunnerCT/actions-runner/_work/CTDemo/CTDemo
+# Build directory: /mnt/c/Users/syoung/Documents/RunnerCT/actions-runner/_work/CTDemo/CTDemo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/mnt/c/Users/syoung/Documents/RunnerCT/actions-runner/_work/CTDemo/CTDemo/build/atm_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
